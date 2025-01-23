5d5c515510a881427a051ab7d1c3439958d0e687
adankevych <adankevych@kse.org.ua>
2025-01-23 22:58:53 +0200

    ticket model

---
+++ CMS/Models/Ticket.cs
@@ -0,0 +1,14 @@
+namespace CMS.Models;
+
+public class Ticket
+{
+    public int TicketId { get; set; }
+    public int UserId { get; set; }
+    public int SessionId { get; set; }
+    public int SeatRow { get; set; }
+    public int PaymentId { get; set; }
+
+    public User User { get; set; } = null!;
+    public Session Session { get; set; } = null!;
+    public Payment Payment { get; set; } = null!;
+}
