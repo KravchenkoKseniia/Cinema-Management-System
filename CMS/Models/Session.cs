5a1b9a03e33a573e5c2bd466dafc9f843a1b93f8
adankevych <adankevych@kse.org.ua>
2025-01-23 22:58:42 +0200

    session model

---
+++ CMS/Models/Session.cs
@@ -0,0 +1,16 @@
+namespace CMS.Models;
+
+public class Session
+{
+    public int SessionId { get; set; }
+    public int MovieId { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public DateTime Date { get; set; }
+    public int HallId { get; set; }
+    public decimal TicketPrice { get; set; }
+
+    public Movie Movie { get; set; } = null!;
+    public Hall Hall { get; set; } = null!;
+    public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
