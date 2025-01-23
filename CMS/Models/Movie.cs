72e17d8c9099ff2d7f9b3900213414dab9332f24
adankevych <adankevych@kse.org.ua>
2025-01-23 22:58:03 +0200

    movie model

---
+++ CMS/Models/Movie.cs
@@ -0,0 +1,17 @@
+namespace CMS.Models;
+
+public class Movie
+{
+    public int MovieId { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string TrailerURL { get; set; } = null!;
+    public DateTime ReleaseDate { get; set; }
+    public int GenreId { get; set; }
+    public decimal Rating { get; set; }
+    public TimeSpan Duration { get; set; }
+    public string PosterURL { get; set; } = null!;
+
+    public Genre Genre { get; set; } = null!;
+    public ICollection<Session> Sessions { get; set; } = new List<Session>();
+}
