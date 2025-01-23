6ae5942e628e414a36a525b2ea49d929d88771e9
adankevych <adankevych@kse.org.ua>
2025-01-23 22:57:53 +0200

    hall model

---
+++ CMS/Models/Hall.cs
@@ -0,0 +1,10 @@
+namespace CMS.Models;
+
+public class Hall
+{
+    public int HallId { get; set; }
+    public string Name { get; set; } = null!;
+    public int Capacity { get; set; }
+
+    public ICollection<Session> Sessions { get; set; } = new List<Session>();
+}
