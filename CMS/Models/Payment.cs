9f5e8ac758dfdcccf5a31e022542929edd17ccfa
adankevych <adankevych@kse.org.ua>
2025-01-23 22:58:16 +0200

    payment model

---
+++ CMS/Models/Payment.cs
@@ -0,0 +1,11 @@
+namespace CMS.Models;
+
+public class Payment
+{
+    public int PaymentId { get; set; }
+    public int UserId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+
+    public User User { get; set; } = null!;
+}
