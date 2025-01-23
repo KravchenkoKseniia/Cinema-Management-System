d4621bd8582c8cf3f9d458f1d5a5588cc0e011ae
adankevych <adankevych@kse.org.ua>
2025-01-23 22:59:05 +0200

    user model

---
+++ CMS/Models/User.cs
@@ -0,0 +1,14 @@
+namespace CMS.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; } = null!;
+    public string HashedPassword { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public int RoleId { get; set; }
+
+    public Role Role { get; set; } = null!;
+    public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+}
