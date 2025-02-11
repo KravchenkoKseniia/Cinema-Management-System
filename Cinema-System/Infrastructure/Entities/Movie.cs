2de2e01ab4ee0f8c9795b381ba59d4d49cb9155e
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-02-12 01:53:31 +0200

    update movie to have list of genre ids

--- Cinema-System/Infrastructure/Entities/Movie.cs
+++ Cinema-System/Infrastructure/Entities/Movie.cs
@@ -9,11 +9,10 @@ public class Movie : IBaseEntity
     public string Description { get; set; } = null!;
     public string TrailerURL { get; set; } = null!;
     public DateTime ReleaseDate { get; set; }
-    public int GenreId { get; set; }
     public decimal Rating { get; set; }
     public TimeSpan Duration { get; set; }
     public string PosterURL { get; set; } = null!;
 
-    public Genre Genre { get; set; } = null!;
+    public ICollection<Genre> Genres { get; set; } = new List<Genre>();
     public ICollection<Session> Sessions { get; set; } = new List<Session>();
 }
