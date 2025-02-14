3d45553626d0d3df8139f8da48625e9733b37feb
adankevych <adankevych@kse.org.ua>
2025-02-15 00:05:41 +0200

    GenreService

---
+++ Cinema-System/Core/Services/GenreService.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Infrastructure.Entities;
+using Cinema_System.Services.Interfaces;
+
+namespace Cinema_System.Services
+{
+    public class GenreService : IGenreService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenreService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Genre>> GetGenresAsync()
+        {
+            return await _context.Genres.ToListAsync();
+        }
+
+        public async Task<Genre?> GetGenreByIdAsync(int id)
+        {
+            return await _context.Genres.FindAsync(id);
+        }
+
+        public async Task AddGenreAsync(Genre genre)
+        {
+            _context.Genres.Add(genre);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
