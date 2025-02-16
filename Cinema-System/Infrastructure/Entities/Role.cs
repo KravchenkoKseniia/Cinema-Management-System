7d792f84054e4b0a5ea31aeef6bca47d7363634d
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-02-16 10:42:21 +0200

    modify Role to inherit from IdentityRole

--- Cinema-System/Infrastructure/Entities/Role.cs
+++ Cinema-System/Infrastructure/Entities/Role.cs
@@ -1,8 +1,9 @@
 using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Entities;
 
-public class Role : IBaseEntity
+public class Role :  IdentityRole<int> 
 {
     public int RoleId { get; set; }
     public string RoleName { get; set; } = null!;
